ff29a25f990551407a966865190d7e5c297bfb0c
nbegany <nbb17291@gmail.com>
2026-09-24 00:35:16 -0400

    Add Midwest Airbnb querychat app

--- apps/job_scout_chat/app.R
+++ apps/job_scout_chat/app.R
@@ -1,3 +1,4 @@
+# ISA 401 Job Scout Chat: ask questions, get SQL, a table, or a chart back
 library(querychat)
 
 con = DBI::dbConnect(RSQLite::SQLite(), "data/scout.db")
@@ -7,13 +8,11 @@ client = ellmer::chat_openai(
   params = ellmer::params(reasoning_effort = "none")
 )
 
-qc = querychat::querychat(
+qc = querychat(
   con, "scout_postings",
   client   = client,
-  tools    = c("filter", "query", "visualize"),
-  greeting = "Ask me about the 1,891 job postings
-              ChatISA Job Scout collected."
+  tools    = c("filter", "query", "visualize"),  # visualize: charts in the chat (needs ggsql)
+  greeting = "Ask me about the 1,891 job postings ChatISA Job Scout collected."
 )
 
 qc$app_obj()
-
